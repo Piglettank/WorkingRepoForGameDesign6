b76ff730476635574cdd690e09539db3f4659cee
Krzemisir <38097972+Krzemisir@users.noreply.github.com>
2020-02-04 14:47:09 +0100

    Single use IGNORE function

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,54 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    GameObject hand;
+    public GameObject[] dots = new GameObject[3];
+    public bool newDude = true;
 
-    void Start()
+    private int waiting = 0;
+    private int dotCount = -1;
+
+
+
+    IEnumerator WaitingCo()
     {
-        
+        for (int a = 0; a <= 2; ++a)
+        {
+            yield return new WaitForSeconds(1);
+            waiting += 1;
+            dotCount += 1;
+            Debug.Log("1s more");
+            Debug.Log("a: " + a);
+            Debug.Log("waiting: " + waiting);
+        }
     }
 
-
-    void Update()
+    IEnumerator EndCo()
     {
-        
+            yield return new WaitForSeconds(1);
+            Debug.Log("IGNORED");
+    }
+
+    void FixedUpdate()
+    {
+
+        if (newDude == true)
+        {
+            StartCoroutine(WaitingCo());
+            newDude = false;
+        }
+
+        if (waiting > 0)
+        {
+            dots[dotCount].SetActive(true);
+        }
+
+        if (waiting >= 3)
+        {
+            StopCoroutine(WaitingCo());
+            Debug.Log("dots done"); 
+            
+            StartCoroutine(EndCo());
+            StopCoroutine(EndCo()); 
+        }
     }
 }
