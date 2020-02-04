bbbdd831f70dce10f2cf175b21b55cfeccaf87a0
Joakim Malmström <malmstrom_9@hotmail.com>
2020-02-04 14:15:24 +0100

    Queue System Functional

--- Assets/Scripts/Dudes.cs
+++ Assets/Scripts/Dudes.cs
@@ -8,7 +8,9 @@ public class Dudes : MonoBehaviour
     public GameObject LineOne, LineTwo, LineThree;
 
     public List<GameObject> DudeList;
-    public List<GameObject> LineList;
+    public List<GameObject> LineListOne;
+    public List<GameObject> LineListTwo;
+    public List<GameObject> LineListThree;
 
     public int linePosition = 0;
 
@@ -19,37 +21,58 @@ public class Dudes : MonoBehaviour
         DudeList.Add(DudeTwo);
         DudeList.Add(DudeThree);
 
-        LineList = new List<GameObject>();
-        LineList.Add(LineOne);
-        LineList.Add(LineTwo);
-        LineList.Add(LineThree);
+        LineListOne = new List<GameObject>();
+        LineListOne.Add(LineThree);
+        LineListOne.Add(LineTwo);
+        LineListOne.Add(LineOne);
+
+        LineListTwo = new List<GameObject>();
+        LineListTwo.Add(LineTwo);
+        LineListTwo.Add(LineOne);
+        LineListTwo.Add(LineThree);
+
+        LineListThree = new List<GameObject>();
+        LineListThree.Add(LineOne);
+        LineListThree.Add(LineThree);
+        LineListThree.Add(LineTwo);
     }
 
     void MoveLine()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            for (int i = 0; i < DudeList.Count; i++)
+            linePosition++;
+            
+            if (linePosition == 1)
             {
-                DudeList[i].transform.position = LineList[i].transform.position;
-                Debug.Log("Dude List: " + DudeList[i].transform.position);
-                Debug.Log("Line List: " + LineList[i].transform.position);
+                for (int i = 0; i < DudeList.Count; i++)
+                {
+                    DudeList[i].transform.position = LineListOne[i].transform.position;
+                }
             }
 
-            //if (DudeOne.transform.position == LineOne.transform.position)
-            //{
-            //    DudeOne.transform.position = LineThree.transform.position;
-            //}
-            //else if (DudeOne.transform.position == LineTwo.transform.position)
-            //{
-            //    DudeOne.transform.position = LineOne.transform.position;
-            //}
-            //else if (DudeOne.transform.position == LineThree.transform.position)
-            //{
-            //    DudeOne.transform.position = LineTwo.transform.position;
-            //}
+            else if (linePosition == 2)
+            {
+                for (int i = 0; i < DudeList.Count; i++)
+                {
+                    DudeList[i].transform.position = LineListTwo[i].transform.position;
+                }
+            }
 
-            Debug.Log("Line moves");
+            else if (linePosition == 3)
+            {
+                for (int i = 0; i < DudeList.Count; i++)
+                {
+                    DudeList[i].transform.position = LineListThree[i].transform.position;  
+                }
+            }
+
+            if (linePosition >= 3)
+            {
+                linePosition = 0;
+            }
+
+            Debug.Log(linePosition);
         }
     }
 
