178499924d7a19c57174db40af4bae69c337b3a3
Krzemisir <38097972+Krzemisir@users.noreply.github.com>
2020-02-04 11:53:02 +0100

    Nothing, really

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    GameObject hand;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
