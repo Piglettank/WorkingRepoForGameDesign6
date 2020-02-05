65f9bbabe8db548330e25324fd521605bc64b4f6
Joakim Malmström <malmstrom_9@hotmail.com>
2020-02-05 12:25:35 +0100

    Cleaned up Dudes Script. Trying to implement reactions

---
+++ Assets/Scripts/HighFiveGuy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighFiveGuy : MonoBehaviour
+{
+    private Animator animator;
+    public int trust = 0;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    //private void HighFiveAction()
+    //{
+    //    if (Dudes.DudeOne.transform.position == Dudes.LineOne.transform.position)
+    //    {
+    //        if (HighFiveTrust(trust) == 0)
+    //        {
+    //            animator.SetBool("mood", true);
+    //        }
+    //    }
+    //}
+
+    public int HighFiveTrust(int t)
+    {
+        if (trust == 0)
+        {
+            // MAKE NEUTRAL FACE
+            // 
+        }
+        if (trust == 1)
+        {
+            // MAKE HAPPY FACE
+        }
+        if (trust == -1)
+        {
+            // MAKE SAD FACE
+        }
+
+        trust = t;
+
+        return t;
+    }
+
+    void Update()
+    {
+        HighFiveTrust(trust);
+        //HighFiveAction();
+    }
+}
