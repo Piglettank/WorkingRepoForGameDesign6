1ec298426cc488ff2016cba2c614d30949714f3c
Joakim Malmström <malmstrom_9@hotmail.com>
2020-02-18 15:50:33 +0100

    Functional Trunk Movement
    Krzesimir: I find that 100 torque in the bones are pretty good. But you need to play around with the spring joints and add the TrunkMovement scripts to more/less bones

---
+++ Assets/Scripts/TrunkMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrunkMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    Vector3 vector;
+
+    public float torque = 100f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        vector = new Vector3(0f, 0f, torque);
+    }
+
+    void Trunk()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddTorque(vector);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddTorque(-vector);
+        }
+            
+    }
+
+    void FixedUpdate()
+    {
+        Trunk();
+    }
+}
