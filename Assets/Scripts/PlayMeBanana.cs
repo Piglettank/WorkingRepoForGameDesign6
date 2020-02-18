27aee1755aa1b5cd47f1019ce5783aa9cac56f93
Krzemisir <38097972+Krzemisir@users.noreply.github.com>
2020-02-18 12:00:43 +0100

    Sound Implemention (not like its working)

---
+++ Assets/Scripts/PlayMeBanana.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMeBanana : MonoBehaviour
+{
+    public void PlaySound()
+    {
+        SoundManager.PlaySound(SoundManager.Sound.bananaBounce0); 
+    }
+
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SoundManager
+{
+    public enum Sound
+    {
+        bananaBounce0,
+        bananaBounce1,
+        bananaBounce2,
+        bananaBounce3,
+        bananaBounce4,
+        bananaBounce5,
+        bananaBounce6,
+        bananaBounce7,
+        bananaBounce8,
+        bananaBounce9,
+        bananaBounce10,
+        bananaBounce11,
+        bananaBounce12,
+        slap,
+    }
+    
+    public static int bounceCount = 0;
+    public static void PlaySound(Sound sound)
+    {
+        GameObject soundGameObject = new GameObject("Sound");
+        AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+        audioSource.PlayOneShot(GetAudioClip(sound));
+    }
+
+    private static AudioClip GetAudioClip(Sound sound)
+    {
+        // LOOK FOR THE CORRECT AUDIO CLIP THAT MATCHES THE SOUND WE NEED
+        foreach (GameAssets.SoundAudioClip soundAudioClip in GameAssets.i.soundAudioClipArray)
+        {
+            if (soundAudioClip.sound == sound)
+            {
+                return soundAudioClip.audioClip;
+            }
+        }
+        Debug.LogError("Sound " + sound + " not found!");
+        return null; 
+    }
+}
