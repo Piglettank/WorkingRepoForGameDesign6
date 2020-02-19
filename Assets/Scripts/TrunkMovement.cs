5dd408aa6aa2c15b6cbc35688219abeb7eceddb8
Joakim Malmström <malmstrom_9@hotmail.com>
2020-02-19 11:01:30 +0100

    Trunk Functionality
    Added colliders in the trunk. Scripts to all the bones (play around with that).
    force variable for trunk force.
    spring variables needs tweaking.

--- Assets/Scripts/TrunkMovement.cs
+++ Assets/Scripts/TrunkMovement.cs
@@ -5,27 +5,57 @@ using UnityEngine;
 public class TrunkMovement : MonoBehaviour
 {
     Rigidbody rb;
-    Vector3 vector;
+    Vector3 torqueVectorZ;
+    Vector3 torqueVectorY;
+    Vector3 torqueVectorX;
+
+    Vector3 forceVectorZ;
+    Vector3 forceVectorY;
+    Vector3 forceVectorX;
 
     public float torque = 100f;
+    public float force = 100f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        vector = new Vector3(0f, 0f, torque);
+        torqueVectorZ = new Vector3(0f, 0f, torque);
+        torqueVectorY = new Vector3(0f, torque, 0f);
+        torqueVectorX = new Vector3(torque, 0f, 0f);
+
+        forceVectorZ = new Vector3(0f, 0f, force);
+        forceVectorY = new Vector3(0f, force, 0f);
+        forceVectorZ = new Vector3(force, 0f, 0f);
+        
     }
 
     void Trunk()
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            rb.AddTorque(vector);
+            // FORCE
+            rb.AddForce(forceVectorZ);
+            //rb.AddForce(forceVectorY);
+            //rb.AddForce(forceVectorX);
+
+            // TORQUE (ROTATION)
+            //rb.AddTorque(vectorZ);
+            //rb.AddTorque(vectorY);
+            //rb.AddTorque(vectorX);
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
-            rb.AddTorque(-vector);
+            // FORCE
+            rb.AddForce(-forceVectorZ);
+            //rb.AddForce(-forceVectorY);
+            //rb.AddForce(-forceVectorX);
+
+            // TORQUE (ROTATION)
+            //rb.AddTorque(-vectorZ);
+            //rb.AddTorque(-vectorY);
+            //rb.AddTorque(-vectorX);
         }
-            
+
     }
 
     void FixedUpdate()
