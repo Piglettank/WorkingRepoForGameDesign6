a1d7b1e2c11dc50c97453c3f7048059eece43797
Joakim Malmström <malmstrom_9@hotmail.com>
2020-02-20 11:45:51 +0100

    Mouse Functionality
    If you want to use the arrows you have to enable it in script (FixedUpdate).

--- Assets/Scripts/TrunkMovement.cs
+++ Assets/Scripts/TrunkMovement.cs
@@ -5,61 +5,46 @@ using UnityEngine;
 public class TrunkMovement : MonoBehaviour
 {
     Rigidbody rb;
-    Vector3 torqueVectorZ;
-    Vector3 torqueVectorY;
-    Vector3 torqueVectorX;
 
-    Vector3 forceVectorZ;
-    Vector3 forceVectorY;
     Vector3 forceVectorX;
 
-    public float torque = 100f;
+    //public float torque = 100f;
     public float force = 100f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        torqueVectorZ = new Vector3(0f, 0f, torque);
-        torqueVectorY = new Vector3(0f, torque, 0f);
-        torqueVectorX = new Vector3(torque, 0f, 0f);
 
-        forceVectorZ = new Vector3(0f, 0f, force);
-        forceVectorY = new Vector3(0f, force, 0f);
-        forceVectorZ = new Vector3(force, 0f, 0f);
-        
+        forceVectorX = new Vector3(force, 0f, 0f);
     }
 
-    void Trunk()
+    void KeyInputTrunk()
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
             // FORCE
-            rb.AddForce(forceVectorZ);
-            //rb.AddForce(forceVectorY);
-            //rb.AddForce(forceVectorX);
+            rb.AddForce(forceVectorX);
 
-            // TORQUE (ROTATION)
-            //rb.AddTorque(vectorZ);
-            //rb.AddTorque(vectorY);
-            //rb.AddTorque(vectorX);
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
             // FORCE
-            rb.AddForce(-forceVectorZ);
-            //rb.AddForce(-forceVectorY);
-            //rb.AddForce(-forceVectorX);
-
-            // TORQUE (ROTATION)
-            //rb.AddTorque(-vectorZ);
-            //rb.AddTorque(-vectorY);
-            //rb.AddTorque(-vectorX);
+            rb.AddForce(-forceVectorX);
         }
+    }
 
+    void MouseInputTrunk()
+    {
+        float horizontal = Input.GetAxis("Mouse X") * force;
+        float vertical = Input.GetAxis("Mouse Y") * force;
+
+        rb.AddForce(0, 0f, -horizontal);
+        rb.AddForce(vertical, 0f, 0f);        
     }
 
     void FixedUpdate()
     {
-        Trunk();
+        //KeyInputTrunk();
+        MouseInputTrunk();
     }
 }
