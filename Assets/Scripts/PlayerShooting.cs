c3126706d9fd468d5637e0bdaf7a63238f79d0ac
Joakim Malmström <malmstrom_9@hotmail.com>
2020-03-09 12:16:43 +0100

    Shooting Implemented. Needs adjusting

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -4,66 +4,27 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    public Transform[] players;
+    public Transform shootingPosition;
+    public Rigidbody projectile;
 
-    private float actionTimer;
+    private int bulletCount = 0;
 
-    public float shootingZoneTwo = 1.5f;
-    public float shootingZoneThree = 3f;
-
-    List<Vector3> playerPosition = new List<Vector3>();
-    
     void Start()
     {
-        playerPosition.Add(players[0].transform.localPosition);
-        playerPosition.Add(players[1].transform.localPosition);
-        playerPosition.Add(players[2].transform.localPosition);
+        
     }
 
     void Shooting()
     {
-
-
-        // THE LOAD UP
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            actionTimer += Time.deltaTime;
-            Debug.Log("" + (int)actionTimer);
-        }
-        // ZONE ONE
-        if (Input.GetKeyUp(KeyCode.Space) && actionTimer < shootingZoneTwo)
-        {
-            //if (Physics.Raycast(weaponPosition, shootingDirection, range))
-            //{
-            //    // something was hit - kill it!
-            //}
-            Debug.Log("Zone 1 fired");
-            actionTimer = 0f;
-        }
-        // ZONE TWO
-        if (Input.GetKeyUp(KeyCode.Space) && actionTimer < shootingZoneThree && actionTimer >= shootingZoneTwo)
-        {
-            Debug.Log("Zone 2 fired");
-            actionTimer = 0f;
-        }
-        // ZONE THREE
-        if (Input.GetKeyUp(KeyCode.Space) && actionTimer >= shootingZoneThree)
-        {
-            Debug.Log("Zone 3 fired");
-            actionTimer = 0f;
+            Instantiate(projectile, shootingPosition.position, Quaternion.Euler(0, 0, 0));
+            Debug.Log("FIRED");
         }
     }
 
     void Update()
     {
         Shooting();
-
-        Vector3 currentPlayerPosition;
-
-        for (int i = 0; i < playerPosition.Count; i++)
-        {
-            currentPlayerPosition = transform.localPosition;
-            Debug.Log(currentPlayerPosition);
-        }
     }
 }
