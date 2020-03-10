5ca3fefc61da5158eed27b2301aa20d0fafe716d
Joakim Malmström <malmstrom_9@hotmail.com>
2020-03-10 12:31:20 +0100

    Shooting Mechanic in the works

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    public Transform shootingPosition;
+    public Transform[] zoneOnePosition;
+    public Transform[] zoneTwoPosition;
+    public Transform[] zoneThreePosition;
+
     public GameObject[] projectileToSpawn;
     private GameObject[] projectileToClone;
 
     public static int bulletCount = 0;
+
+    private float actionTimer = 0f;
+    private float zoneTwoTimer = 1.5f;
+    private float zoneThreeTimer = 3f;
 
     private int blueFirstPower = 2;
     private int blueSecondPower = 4;
@@ -21,28 +28,68 @@ public class PlayerShooting : MonoBehaviour
 
     void Shooting()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && bulletCount < 1)
+        
+        if (Input.GetKey(KeyCode.Space) && bulletCount < 1)
         {
-            //if (Bullet.bluePower < blueFirstPower)
-            //{
-            //    // SPAWNS FIRST BULLET
-            //}
-            //else if (Bullet.bluePower >= blueSecondPower && Bullet.bluePower < blueThirdPower)
-            //{
-            //    // SPAWNS SECOND BULLET
-            //}
-            //else if (Bullet.bluePower >= blueThirdPower)
-            //{
-            //    // SPAWNS THIRD BULLET
-            //}
-            // SPAWNS BULLETS AND INCREMENTS bulletCount
-            for (int i = 0; i < projectileToSpawn.Length; i++)
+            actionTimer += Time.time;
+            Debug.Log(actionTimer);
+
+            if (Input.GetKeyUp(KeyCode.Space) && actionTimer < zoneTwoTimer)
             {
-                projectileToClone[i] = Instantiate(projectileToSpawn[i], shootingPosition.position, Quaternion.Euler(0, 0, 0)) as GameObject;
-                bulletCount++;
+                for (int i = 0; i < projectileToSpawn.Length; i++)
+                {
+                    projectileToClone[i] = Instantiate(projectileToSpawn[i], zoneOnePosition[0].position, Quaternion.Euler(0, 0, 0)) as GameObject;
+                    projectileToClone[i] = Instantiate(projectileToSpawn[i], zoneOnePosition[1].position, Quaternion.Euler(0, 0, 0)) as GameObject;
+                    projectileToClone[i] = Instantiate(projectileToSpawn[i], zoneOnePosition[2].position, Quaternion.Euler(0, 0, 0)) as GameObject;
+                    bulletCount++;
+                    Debug.Log("ZONE 1 FIRED");
+
+                    actionTimer = 0f;
+                }
             }
-            Debug.Log("FIRED");
+
+            else if (Input.GetKeyUp(KeyCode.Space) && actionTimer >= zoneTwoTimer || actionTimer < zoneThreeTimer)
+            {
+                for (int i = 0; i < projectileToSpawn.Length; i++)
+                {
+                    projectileToClone[i] = Instantiate(projectileToSpawn[i], zoneTwoPosition[0].position, Quaternion.Euler(0, 0, 0)) as GameObject;
+                    projectileToClone[i] = Instantiate(projectileToSpawn[i], zoneTwoPosition[1].position, Quaternion.Euler(0, 0, 0)) as GameObject;
+                    projectileToClone[i] = Instantiate(projectileToSpawn[i], zoneTwoPosition[2].position, Quaternion.Euler(0, 0, 0)) as GameObject;
+                    bulletCount++;
+                    Debug.Log("ZONE 2 FIRED");
+
+                    actionTimer = 0f;
+                }
+            }
+
+            else if (Input.GetKeyUp(KeyCode.Space) && actionTimer >= zoneThreeTimer)
+            {
+                for (int i = 0; i < projectileToSpawn.Length; i++)
+                {
+                    Debug.Log("Zone 3 FIRED");
+                    projectileToClone[i] = Instantiate(projectileToSpawn[i], zoneThreePosition[0].position, Quaternion.Euler(0, 0, 0)) as GameObject;
+                    bulletCount++;
+
+                    actionTimer = 0f;
+                }
+            }
         }
+
+        
+
+        //if (Bullet.bluePower < blueFirstPower)
+        //{
+        //    // SPAWNS FIRST BULLET
+        //}
+        //else if (Bullet.bluePower >= blueSecondPower && Bullet.bluePower < blueThirdPower)
+        //{
+        //    // SPAWNS SECOND BULLET
+        //}
+        //else if (Bullet.bluePower >= blueThirdPower)
+        //{
+        //    // SPAWNS THIRD BULLET
+        //}
+        // SPAWNS BULLETS AND INCREMENTS bulletCount
     }
 
     void Update()
