1ac763083654ef2edd7aad098fb821dcfe03e189
Pig <johanfallberg@hotmail.com>
2020-03-10 16:56:00 +0100

    shitty fucking animation goddammit theres too much I can what

---
+++ Assets/Scripts/JohansShittyAnimationScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JohansShittyAnimationScript : MonoBehaviour
+{
+    public GameObject circle;
+    Animator playerAnimator;
+
+    void Start()
+    {
+        //Get Animator
+        playerAnimator = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //Set joakimRotation to the z rotation of the Gucci Gang
+        playerAnimator.SetFloat("joakimRotation", 360 - circle.transform.eulerAngles.y);
+        Debug.Log(circle.transform.rotation.z);
+    }
+}
