9aa3c8e4cc6ca345eafde24086f8b69814d6b69e
Krzemisir <38097972+Krzemisir@users.noreply.github.com>
2020-03-11 19:58:45 +0100

    Improved Range Indicators, Skiers MovementOnBeat

--- Assets/Scripts/SkierMovement.cs
+++ Assets/Scripts/SkierMovement.cs
@@ -1,38 +1,70 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SkierMovement : MonoBehaviour
 {
 
 	[SerializeField]
-	float XmoveSpeed = 0.5f;
+	public static float XmoveSpeed = 1f;
 
 	[SerializeField]
-	float YmoveSpeed = 0.5f;
+	public static float YmoveSpeed = 0f;
 
 	[SerializeField]
-	float frequency = 0.5f;
+	public static float frequency = 2f;
 
 	[SerializeField]
-	float magnitude = 0.5f;
+	public static float magnitude = 2f;
 
 	Vector3 pos, localScale;
 
+	float beatTimer = 0;
+	bool onBeat = false;
+	public bool onBeatWait = false;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		onBeatWait = true;
+	}
 
 	void Start()
 	{
-
 		pos = transform.position;
-
 		localScale = transform.localScale;
-
 	}
 
-	void Update()
+
+	private void FixedUpdate()
 	{
-		MoveRight();
+		if (onBeatWait) beatTimer += Time.deltaTime;
+
+		if (onBeat)
+		{
+			XmoveSpeed = 2f;
+			frequency = 6f;
+			magnitude = 1f;
+			MoveRight();
+		}
+
+		
+		if (onBeatWait && beatTimer % 0.5 <= 0.1f)
+		{
+			Debug.Log("beatTimer % 2 > 0.1f");
+			MoveRight();
+			onBeat = true;
+			onBeatWait = false;
+			beatTimer = 0f;
+		}
+		else
+		{
+			MoveRight();
+			XmoveSpeed = 1f;
+			frequency = 2f;
+			magnitude = 2f;
+		}
+
+
 	}
+
 
 	void MoveRight()
 	{
