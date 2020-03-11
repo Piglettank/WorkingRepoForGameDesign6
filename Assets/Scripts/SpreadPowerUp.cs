91d4eba49f9da7e68d7d72b854432aa5e569bc84
Joakim Malmström <malmstrom_9@hotmail.com>
2020-03-11 15:48:12 +0100

    Spread Power Up Foundation
    Everything works except the scaling of the projectile. But the players go further apart and the range indicator scales up as well.
    
    PRESS F to try it

--- Assets/Scripts/SpreadPowerUp.cs
+++ Assets/Scripts/SpreadPowerUp.cs
@@ -5,20 +5,71 @@ using UnityEngine;
 public class SpreadPowerUp : MonoBehaviour
 {
     public Transform[] playerPosition;
+    public Transform[] rangePositionOne = new Transform[3];
+    public Transform[] rangePositionTwo = new Transform[3];
+    public Transform[] rangeIndicator = new Transform[7];
+
+    private Vector3[] scaleChange = new Vector3[3];
+    private Vector3[] scaleRangeChangeOne = new Vector3[3];
+    private Vector3[] scaleRangeChangeTwo = new Vector3[3];
+    private Vector3 scaleIndicatorChange;
+
+    public float spreadIncrease = 0.10f;
+
+    private float spreadRangeIncreaseOne = 0.5f;
+    private float spreadRangeIncreaseTwo = 0.25f;
+    private float rangeIndicatorIncrease = 0.5f;
 
     void Start()
     {
-        
+        // JOAKIM CHARACTER
+        scaleChange[0] = new Vector3(0f, spreadIncrease, 0f);
+        // JOHAN CHARACTER
+        scaleChange[1] = new Vector3(spreadIncrease, -spreadIncrease, 0f);
+        // KRZESIMIR CHARACTER
+        scaleChange[2] = new Vector3(-spreadIncrease, -spreadIncrease, 0f);
+
+        // ZONE ONE CHANGE
+        scaleRangeChangeOne[0] = new Vector3(0f, 0f, spreadRangeIncreaseOne);
+        scaleRangeChangeOne[1] = new Vector3(spreadRangeIncreaseOne, 0f, -spreadRangeIncreaseOne);
+        scaleRangeChangeOne[2] = new Vector3(-spreadRangeIncreaseOne, 0f, -spreadRangeIncreaseOne);
+
+        // ZONE TWO CHANGE
+        scaleRangeChangeTwo[0] = new Vector3(0f, 0f, spreadRangeIncreaseTwo);
+        scaleRangeChangeTwo[1] = new Vector3(spreadRangeIncreaseTwo, 0f, -spreadRangeIncreaseTwo);
+        scaleRangeChangeTwo[2] = new Vector3(-spreadRangeIncreaseTwo, 0f, -spreadRangeIncreaseTwo);
+
+        // RANGE INDICATOR CHANGE
+        scaleIndicatorChange = new Vector3(rangeIndicatorIncrease, 0f, rangeIndicatorIncrease);
     }
 
     void Spread()
     {
-        if (HitIndicator.spreadPower == 1)
+        // if (HitIndicator.spreadPower == 2)
+        // TEMPORARY INPUT F
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            // WORKS BUT MAYBE DO IT LIKE A BOOL
-            playerPosition[0].transform.position = new Vector3(transform.position.x, transform.position.y + 3, transform.position.z);
+            // INCREASE THE SPREAD
+            for (int i = 0; i < scaleChange.Length; i++)
+            {
+                playerPosition[i].transform.localPosition += scaleChange[i];
+            }
+            // INCREASE THE SPREAD FOR RANGE ONE
+            for (int i = 0; i < rangePositionOne.Length; i++)
+            {
+                rangePositionOne[i].transform.localPosition += scaleRangeChangeOne[i];
+            }
+            // INCREASE THE SPREAD FOR RANGE TWO
+            for (int i = 0; i < rangePositionTwo.Length; i++)
+            {
+                rangePositionTwo[i].transform.localPosition += scaleRangeChangeTwo[i];
+            }
             Debug.Log("Increase SPREAD");
-            HitIndicator.spreadPower = 2;
+            for (int i = 0; i < rangeIndicator.Length; i++)
+            {
+                rangeIndicator[i].transform.localScale += scaleIndicatorChange;
+            }
+            //HitIndicator.spreadPower = 2;
         }
     }
 
